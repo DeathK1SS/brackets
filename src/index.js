174273b979634c0cd4915422698d5608e7276e57
v2.kaminsky <v2.kaminsky@itransition.com>
2020-10-25 19:45:41 +0300

    feat: added solution for task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+   let strSequence = str.split('');
+
+    if (strSequence.length % 2 !== 0) return false;
+
+    for (let i = 0; i < strSequence.length; i++) {
+        bracketsConfig.forEach(item => {
+            if (strSequence[i] === item[0] && strSequence[i + 1] === item[1]) {
+                strSequence.splice(i, 2);
+                i = -1;
+            }
+        })
+    }
+
+    return strSequence.length === 0;
 }
